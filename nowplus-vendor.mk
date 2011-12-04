5c094ee6b40d8414f8d32a08bac5f56e32024055
r3d4 <terrae@gmx.de>
2011-12-04 14:23:46 +0100

    remove libbridge filecopy (now build from source) re-add live wallpaper
    Change-Id: I37c76f5aa3ea0bd0bb89a844bf6ef9250e63596e

--- nowplus-vendor.mk
+++ nowplus-vendor.mk
@@ -13,16 +13,16 @@
 # limitations under the License.
 
 # Live wallpaper packages and Themes
-#PRODUCT_PACKAGES := \
-#    LiveWallpapers \
-#    LiveWallpapersPicker \
-#    MagicSmokeWallpapers \
-#    VisualizationWallpapers \
-#    librs_jni
+PRODUCT_PACKAGES := \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni
 
 # Publish that we support the live wallpaper feature.
-#PRODUCT_COPY_FILES := \
-#    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+PRODUCT_COPY_FILES := \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/nowplus/overlay
